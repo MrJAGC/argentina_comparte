9e172b0b0e90b10067cabc37d7fbb0e6b0943c28
alejo <juanalejandrogc@hotmail.es>
2026-05-07 18:28:09 -0500

    first commit

---
+++ src/main/java/com/usta/argentina_comparte/ArgentinaComparteApplication.java
@@ -0,0 +1,13 @@
+package com.usta.argentina_comparte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArgentinaComparteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArgentinaComparteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/usta/argentina_comparte/ArgentinaComparteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usta.argentina_comparte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArgentinaComparteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
